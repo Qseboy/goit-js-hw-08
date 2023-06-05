06f8f5c7943b2a05f95564d877a0cec75dd21c45
Nikolay Shein <qseboy1998@gmail.com>
2023-06-05 13:44:01 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+
+const TIME_LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(time => {
+    localStorage.setItem(TIME_LOCALSTORAGE_KEY, time.seconds);
+  }, 1000)
+);
+
+if (localStorage.getItem(TIME_LOCALSTORAGE_KEY)) {
+  player.setCurrentTime(localStorage.getItem(TIME_LOCALSTORAGE_KEY));
+}
